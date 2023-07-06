44232c1644e2fb221b5ffdbaa2848668373fd2d0
Jung ChanYeong <jcy0342@gmail.com>
2023-07-06 15:29:07 +0900

    [init] : 위버 데이터 저장 서버 틀 생성

---
+++ weiver-data-server/src/test/java/com/weiverdataserver/WeiverDataServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weiverdataserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeiverDataServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
