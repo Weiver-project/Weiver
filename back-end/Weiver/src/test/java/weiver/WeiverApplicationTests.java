4a48edd75aa1425fa816044ea206efa0fb7e3fdf
Jung ChanYeong <jcy0342@gmail.com>
2023-07-06 16:40:58 +0900

    [init] 메인 서버 및 데이터 저장 서버 초기 설정 폴더 생성

---
+++ back-end/Weiver/src/test/java/weiver/WeiverApplicationTests.java
@@ -0,0 +1,13 @@
+package weiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ back-end/data-sever/src/test/java/datasever/DataSeverApplicationTests.java
@@ -0,0 +1,13 @@
+package datasever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataSeverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
