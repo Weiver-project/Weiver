ceb8187b0f858b00f2b566d68dd667b8a6b54f77
ha2sted <dltjrgus4@naver.com>
2023-07-08 16:48:40 +0900

    [크롤링 중간 저장]

--- back-end/data-sever/src/main/java/entity/Actor.java
+++ back-end/data-sever/src/main/java/entity/Actor.java
@@ -1,13 +1,23 @@
 package entity;
 
+import java.util.ArrayList;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Builder;
+
 @Document(collection = "actor")
+@Builder
 public class Actor {
 	@Id
 	private String _id;
 	private String name;
 	private String profileImage;
 	private Casting[] castings;
+	private ArrayList<Casting> Acastings;
+	
+	public void insertRole(Casting role) {
+		Acastings.add(role);
+	}
 }
--- back-end/data-sever/src/main/java/entity/Casting.java
+++ back-end/data-sever/src/main/java/entity/Casting.java
@@ -5,7 +5,16 @@ import java.util.Date;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 @Document(collection = "casting")
+@Builder
+@Getter
+@ToString
+@Setter
 public class Casting {
 	@Id
 	private String _id;
