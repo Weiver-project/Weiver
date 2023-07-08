d040070cd11fd2d5213c5e0a67ad4f4077ee3f27
suby-kwak <rhkdbtj@gmail.com>
2023-07-08 16:50:20 +0900

    [Remove]: User 기능 삭제

--- back-end/Weiver/src/main/java/weiver/controller/UserController.java
+++ back-end/Weiver/src/main/java/weiver/controller/UserController.java
@@ -6,37 +6,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import weiver.service.UserService;
 
 @Controller
 public class UserController {
 
-	@Autowired
-	private UserService userService;
 
 	@RequestMapping(value="/mongotest",method = RequestMethod.GET)
 	public void test() {
-		userService.findByAdmin(false);
-		userService.findByIdAndAdmin("qwer1234@daum.com", false);
 	}
 	
 	@RequestMapping(value="/mongo",method = RequestMethod.GET)
-	public void updatetest(@RequestParam String id,
-								@RequestParam String password,
-								@RequestParam String img,
-								@RequestParam String name) {
-		
-		if(password != null && password != "") {
-			userService.updatePassword(id, password);
-		}
-		
-		if(img != null && img != "") {
-			userService.updateImg(id, img);
-		}
-		
-		if(name != null && name != "") {
-			userService.updateName(id, name);
-		}
+	public void updatetest() {
 		
 	}
 }
