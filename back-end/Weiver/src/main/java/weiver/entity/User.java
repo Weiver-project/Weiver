66098a9ee17e6907ca9b8c5b0d8f3b1b822c2050
suby-kwak <rhkdbtj@gmail.com>
2023-07-12 17:33:03 +0900

    [Refactor]: 유저 entity 수정

--- back-end/Weiver/src/main/java/weiver/entity/User.java
+++ back-end/Weiver/src/main/java/weiver/entity/User.java
@@ -2,12 +2,16 @@ package weiver.entity;
 
 import javax.persistence.*;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Entity
+@Builder
 @Table(name = "user_tb")
 public class User {
 	
