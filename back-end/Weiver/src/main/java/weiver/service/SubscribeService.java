ff0b69216d89d7565643005fdc7d79f7d5644b82
정찬영 <jcy0342@gmail.com>
2023-07-18 11:55:58 +0900

    [fix] subscribe repository 쿼리 에러 해결

--- back-end/Weiver/src/main/java/weiver/service/SubscribeService.java
+++ back-end/Weiver/src/main/java/weiver/service/SubscribeService.java
@@ -16,28 +16,28 @@ public class SubscribeService {
 
 
     /*찜, 봤어요 뮤지컬 조회*/
-//    public List<SimpleMusicalDTO> getSubscribeMusical(String userId, String type){
-//        List<SimpleMusicalDTO> simpleMusicalDTOS = new ArrayList<>();
-//
-//        List<Object[]> objects = subscribeRepository.findMusicalIdByUserIdAndType(userId, type);
-//
-//        for(Object[] o : objects){
-//            String id = (String) o[0];
-//            String title = (String) o[1];
-//            String poster = (String) o[2];
-//            Date stDate = (Date) o[3];
-//            Date edDate = (Date) o[4];
-//
-//            SimpleMusicalDTO simpleMusicalDTO = SimpleMusicalDTO.builder()
-//                    .id(id)
-//                    .title(title)
-//                    .posterImage(poster)
-//                    .stDate(stDate)
-//                    .edDate(edDate)
-//                    .build();
-//
-//            simpleMusicalDTOS.add(simpleMusicalDTO);
-//        }
-//        return simpleMusicalDTOS;
-//    }
+    public List<SimpleMusicalDTO> getSubscribeMusical(String userId, String type){
+        List<SimpleMusicalDTO> simpleMusicalDTOS = new ArrayList<>();
+
+        List<Object[]> objects = subscribeRepository.findMusicalIdByUserIdAndType(userId, type);
+
+        for(Object[] o : objects){
+            String id = (String) o[0];
+            String title = (String) o[1];
+            String poster = (String) o[2];
+            Date stDate = (Date) o[3];
+            Date edDate = (Date) o[4];
+
+            SimpleMusicalDTO simpleMusicalDTO = SimpleMusicalDTO.builder()
+                    .id(id)
+                    .title(title)
+                    .posterImage(poster)
+                    .stDate(stDate)
+                    .edDate(edDate)
+                    .build();
+
+            simpleMusicalDTOS.add(simpleMusicalDTO);
+        }
+        return simpleMusicalDTOS;
+    }
 }
