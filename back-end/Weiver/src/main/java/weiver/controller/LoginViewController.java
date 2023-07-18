604906f8c7be35e6ea3fdc7ce6f4288d5fe7dce9
JongHyeong <leejhjj90@gmail.com>
2023-07-18 10:30:17 +0900

    [login] 수정중

--- back-end/Weiver/src/main/java/weiver/controller/LoginViewController.java
+++ back-end/Weiver/src/main/java/weiver/controller/LoginViewController.java
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginViewController {
 	// 로그인 페이지
-	@GetMapping(value = "/loginPage")
+	@GetMapping(value = "/login")
 	public String loginPage() {
 		return "login";
 	}
 	
 	// 회원가입 페이지
-	@GetMapping(value = "/signupPage")
+	@GetMapping(value = "/signup")
 	public String signupPage() {
 		return "signup";
 	}
--- back-end/data-sever/src/main/java/com/example/entity/Musical.java
+++ back-end/data-sever/src/main/java/com/example/entity/Musical.java
@@ -22,26 +22,28 @@ public class Musical {
 	@Id
 	@Column(name = "ID")
 	private String id;
+	
 	@Column(name = "TITLE")
 	private String title;
-	@Nullable
+	
 	@Column(name = "THEATER")
 	private String theater;
+	
 	@Column(name = "POSTER_IMAGE")
 	private String posterImage;
+	
 	@Column(name = "STDATE")
-	@Nullable
 	private Date stDate;
+	
 	@Column(name = "EDDATE")
-	@Nullable
 	private Date edDate;
 	@Column(name = "VIEW_AGE")
-	@Nullable
+	
 	private String viewAge;
 	@Column(name = "RUNNING_TIME")
-	@Nullable
+	
 	private String runningTime;
-	@Nullable
+	
 	@Column(name = "MAIN_CHARACTER")
 	private String mainCharacter;
 	
