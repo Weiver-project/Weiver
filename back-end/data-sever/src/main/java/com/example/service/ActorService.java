ff89b207e69720cfb192975e1812bf5dbd5c5900
ha2sted <dltjrgus4@naver.com>
2023-07-18 09:41:33 +0900

    [feat] MusicalId로 Actor, Casting 저장하는 메소드 구현

--- back-end/data-sever/src/main/java/com/example/service/ActorService.java
+++ back-end/data-sever/src/main/java/com/example/service/ActorService.java
@@ -1,6 +1,7 @@
 package com.example.service;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -11,8 +12,11 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.constant.Sub_category;
+import com.example.constant.URLs;
 import com.example.entity.Actor;
 import com.example.entity.Casting;
 import com.example.entity.Musical;
@@ -21,183 +25,114 @@ import com.example.repository.CastingRepository;
 import com.example.repository.MusicalRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 
 @Service
 @RequiredArgsConstructor
 public class ActorService {
-
-	private final ActorRepository actorRepository;
-	private final CastingRepository castingRepository;
-	private final MusicalRepository musicalRepository;
-
-	// 배우 DB에 정보 추가
-	public void saveActor() {
-		// 최신화 하기 전에 기존 데이터를 모두 삭제한다.
-//		castingRepository.deleteAll();
-//		actorRepository.deleteAll();
-//		musicalRepository.deleteAll();
+	@Autowired
+	private ActorRepository actorRepository;
+	@Autowired
+	private CastingRepository castingRepository;
+	
+	// Musical에서 배우 정보를 가져와 하나씩 추가
+	public void saveData(String musicalId) {	
+		String musicalURL = "http://www.playdb.co.kr/playdb/playdbDetail.asp?sReqPlayno=" + musicalId;
 		
 		Document doc = null;
-		// Casting.title에서 " - 지역이름" 을 제외하기 위한 키워드
-		List<String> regions = new ArrayList<String>();
-
+		List<String> actorIds = new ArrayList<String>();
+		
 		try {
-
-			String mainURL = "http://www.playdb.co.kr";
-			String mainURL2 = "http://www.playdb.co.kr/artistdb/";
-			// 뮤지컬 배우 첫 페이지
-
-			int pageNo = 1; // 초기 페이지 넘버
-			String ActorListURL = "https://www.playdb.co.kr/artistdb/list_iframe.asp?Page=" + pageNo + "&code=013003&sub_code=&ImportantSelect=&ClickCnt=Y&NameSort=&Country=Y&TKPower=&WeekClickCnt=&NameStart=&NameEnd=";
-
-			// 뮤지컬 배우 첫 페이지 접근
-			doc = Jsoup.connect(ActorListURL).get();
-
-			// 최대 페이지 알아내기
-			Elements maxPages = doc.select("body > table > tbody > tr > td");
-			int maxPage = Integer.parseInt(maxPages.get(maxPages.size()-1).text().split("/")[1].split("]")[0]);
-
-			// 페이지 반복
-			for(pageNo = 1; pageNo <= maxPage; pageNo++) {
-				System.out.println("지금부터 " + pageNo + "페이지 데이터 처리 시작");
-
-				// 페이지별로 배우 리스트 접근
-				ActorListURL = "https://www.playdb.co.kr/artistdb/list_iframe.asp?Page=" + pageNo + "&code=013003&sub_code=&ImportantSelect=&ClickCnt=Y&NameSort=&Country=Y&TKPower=&WeekClickCnt=&NameStart=&NameEnd=";
-				doc = Jsoup.connect(ActorListURL).get();
-
-				// 각각 배우 상세로 들어가는 방법
-				Elements imgSrcs = doc.select("body > table > tbody > tr > td > table > tbody > tr > td > table > tbody > tr > td > a");
-
-				// 모든 배우에 대한 반복
-				for(Element imgSrc : imgSrcs) {
-					if((imgSrc.text()).equals("")) {
-						String actorDetailURL = mainURL2 + imgSrc.attr("href");
-						
-						// 새로 연결
-						doc = Jsoup.connect(actorDetailURL).get();
-
-						// 배우 이미지 주소
-						String actorImage = doc.select("#manImage").attr("src");
-
-						// 배우 이름
-						String actorName = doc.select(".title").get(0).text();
-
-						// 출연 작품 : 이미지 .img_size4
-						// iframe 접근을 위해 actorNo값을 알아낸다.
-						String contentImageURL = "#detailcontents > iframe#iFrmContent";
-						String contentactorNo = (doc.select(contentImageURL).attr("src")).split("=")[2];
-
-						// iframeURL
-	//					http://www.playdb.co.kr/artistdb/Detail_Content_new.asp?TabKind=3&ManNo=45997
-						String contentIframeURL = mainURL2 + "Detail_Content_new.asp?TabKind=3&ManNo=" + contentactorNo;
-						// contentIframeURL로 연결
-						doc = Jsoup.connect(contentIframeURL).get();
-
-						// 전체 Casting 객체 생성
-						ArrayList<Casting> actorAllCastings = new ArrayList<Casting>();
-						
-						// 출연 작품 이미지 정보
-						String tableImageURL = ".detail_contentsbox > table > tbody > tr > td > table > tbody > tr > td > a > img.img_size4";
-						Elements images = doc.select(tableImageURL);
-						
-						Actor a = new Actor();
-						a.setId(contentactorNo);
-						for(Element image : images) {
-							Musical m = new Musical();
-							m.setId(image.attr("src").split("_")[1]);
-							// 출연 작품들에 대한 ID값들을 순차적으로 갖고 있다.
-							actorAllCastings.add(Casting.builder()
-														.actorId(a)
-														.musicalId(m)
-														.build());
-						}
-						
-						// 출연 작품 정보 (이미지 제외)
-						String tableRoleURL = ".detail_contentsbox > table > tbody > tr > td > table > tbody > tr > td > table > tbody > tr > td";
-						Elements roles = doc.select(tableRoleURL);
-						for(int i = 0; i < roles.size(); i++) {
-
-							switch(i % 5) {
-								// 출연 작품 Title
-								case(0):
-									String title = roles.get(i).text();
-									break;
-								// 출연 작품 Date
-								case(1):
-									String date = roles.get(i).text();
-									break;
-								// 출연 작품 장소
-								case(2):
-									String theater = roles.get(i).text();
-									break;
-								// 출연 작품 Role
-								case(3):
-									String role = roles.get(i).text();
-									actorAllCastings.get(i/5).setRole(role);
-									break;
-								// 여백 처리
-								case(4):
-									String blank = roles.get(i).text();
-									break;
-							} // switch
-
-						} // for
-						
-						// 가져올 정보
-//						배우 ID : contentactorNo
-//						배우 이미지 주소 : actorImage
-//						배우 이름 : actorName.split(" ")[0]
-//						출연 작품 정보 : actorCastings
-
-						// Casting에서 뮤지컬만 골라내기
-						// "역" Casting 객체 생성
-						ArrayList<Casting> actorCastings = new ArrayList<Casting>();
-						for(Casting c : actorAllCastings) {
-							if(c.getRole().endsWith("역")) {
-								actorCastings.add(c);
-//								System.out.println(c);
-							}
-						}
-						
-						// Actor 객체 생성
-						Actor actor = Actor.builder()
-											.id(contentactorNo)
-											.name(actorName.split(" ")[0])
-											.profileImage(actorImage)
-											.build();
-//						System.out.println(actor);
-
-						
-						// 배우DB에 저장
-						//System.out.println();
-						actorRepository.save(actor);
-//						System.out.println("Actor 저장 완료");
-						
-						// 뮤지컬 임시 DB 저장
-//						for(Casting c : actorAllCastings) {
-//							Musical musical = Musical.builder()
-//													.id(c.getMusicalId().getId())
-//													.stDate(new Date())
-//													.edDate(new Date())
-//													.title("title")
-//													.theater("theater")
-//													.posterImage("image")
-//													.build();
-//							musicalRepository.save(musical);
-//						}
-						
-						// CastingDB에 저장
-//						castingRepository.saveAll(actorCastings);
-//						System.out.println("Casting 저장 완료");
-					}
+			doc = Jsoup.connect(musicalURL).get();
+			
+			// 배우 역할 List를 가져온다.
+			String actorRoleListTag = ".detail_contentsbox > table > tbody > tr > td > table > tbody > tr";
+			Elements actorRoleList = doc.select(actorRoleListTag);
+			
+			// Role에 따라 배우들을 분리한다.
+			for(Element e : actorRoleList) {
+				// 어떤 Role에 대해서 처리하는가?
+				String role = e.select("b").text();
+				
+				Elements musicalHtml = e.select("img");
+				
+				// 해당 Role에 맞는 배우 ID를 actorIds에 저장한다.
+				for(int i = 0; i < musicalHtml.size(); i++) {
+					String actorId = musicalHtml.get(i).attr("src").split("_")[1];
+					actorIds.add(actorId);
 				}
+				
+				// actorIds를 순환하여 ActorDB 저장, CastingDB 저장
+				for(String actorId : actorIds) {
+					saveActor(actorId);
+					saveCasting(actorId, musicalId, role);
+				}
+				actorIds.clear();
 			}
-			System.out.println("모든 insert 종료");
-
+			
 		} catch (IOException e) {
 			e.printStackTrace();
+		} // catch
+	}// saveActor()
+	
+	// actor : id + name + img
+	private void saveActor(String actorId) {
+		// 기존에 없는 Actor만 저장한다.
+		if(actorRepository.findById(actorId) == null) {
+			Document doc = null;
+			String mainURL2 = "http://www.playdb.co.kr/artistdb/detail.asp?ManNo=";
+			String actorDetailURL = mainURL2 + actorId;
+			
+			// 새로 연결
+			try {
+				doc = Jsoup.connect(actorDetailURL).get();
+				
+				// 배우 이미지 주소
+				String actorImage = doc.select("#manImage").attr("src");
+		
+				// 배우 이름
+				String actorName = doc.select(".title").get(0).text();
+		
+				// ===================== Actor 객체 생성 ==========================
+				Actor actor = Actor.builder()
+						.id(actorId)
+						.name(actorName.split(" ")[0])
+						.profileImage(actorImage)
+						.build();
+				System.out.println(actor);
+				
+				actorRepository.save(actor);
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}else { // 배우가 이미 존재하는 경우
+			System.out.println("이미 존재하는 Actor입니다.");
 		}
-
 	}
-
+	
+	// casting : id(시퀀스) + actor + musical + role
+	private void saveCasting(String actorId, String musicalId, String role) {
+		Actor actor = Actor.builder()
+							.id(actorId)
+							.name("")
+							.build();
+		Musical musical = Musical.builder()
+								.id(musicalId)
+								.build();
+		
+		if(castingRepository.findByActorIdAndMusicalId(actor, musical).size() == 0){
+			Casting casting = Casting.builder()
+									.actorId(actor)
+									.musicalId(musical)
+									.role(role)
+									.build();
+	
+			System.out.println(casting);
+			castingRepository.save(casting);
+		}else {
+			System.out.println("이미 존재하는 Casting입니다.");
+		}
+	}
+	
 }
