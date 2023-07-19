ff18e8d03afa8cd228f641d95c30553e80656210
JongHyeong <leejhjj90@gmail.com>
2023-07-19 14:05:41 +0900

    [feature] 뮤지컬 상세 페이지 출연진 정보 구현 완료1

--- back-end/Weiver/src/main/java/weiver/service/ActorService.java
+++ back-end/Weiver/src/main/java/weiver/service/ActorService.java
@@ -25,13 +25,15 @@ public class ActorService {
 		
 		return actor;
 	}
-
+	
+	// 배우 상세 페이지 뮤지컬 리스트 조회
 	public List<SimpleMusicalDTO> getmusicalListByActorId(String actorId) throws Exception{
 		List<SimpleMusicalDTO> musicalPosterImgList = musicalRepository.findMusicalsByActorId(actorId);
 		
 		return musicalPosterImgList;
 	}
-
+	
+	// 뮤지컬 상세 페이지 캐스팅 조회
 	public List<ResponseCastingDTO> getCastingByMusicalId(String id) {
 		List<ResponseCastingDTO> casting = castingRepository.getCastingByMusicalId(id);
 		return casting;
