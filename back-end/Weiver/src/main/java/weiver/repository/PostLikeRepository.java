9510d9a5d5fe5240b1a42c42c5c9bb689baa0cec
suby-kwak <rhkdbtj@gmail.com>
2023-07-20 11:08:29 +0900

    [Refactor]: 게시물 조회 에러 수정

--- back-end/Weiver/src/main/java/weiver/repository/PostLikeRepository.java
+++ back-end/Weiver/src/main/java/weiver/repository/PostLikeRepository.java
@@ -2,20 +2,20 @@ package weiver.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
-
 import weiver.entity.PostLike;
 
-import java.util.List;
-
 import javax.transaction.Transactional;
+import java.util.List;
 
 public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
     List<PostLike> findByUserId(String userId);
 
     int countByUserId(String userId);
 
+	int countByPostId(Long id);
+
 	PostLike getPostLikeById(Long id);
-	
+
 	@Modifying
 	@Transactional
 	int deletePostLikeById(Long id);
