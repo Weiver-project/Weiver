f1f5e3740df865dd47da686b52a07184c9af6793
JongHyeong <leejhjj90@gmail.com>
2023-07-22 16:04:09 +0900

    [fix] 뮤지컬 조회 최신순으로, 배우 상세 페이지 타이틀 추가

--- back-end/Weiver/src/main/java/weiver/repository/MusicalRepository.java
+++ back-end/Weiver/src/main/java/weiver/repository/MusicalRepository.java
@@ -22,7 +22,7 @@ public interface MusicalRepository extends JpaRepository<Musical, String> {
 	 * regex로 title의 값에 keyword가 포함하는지 확인하는데, 
 	 * 이때 옵션으로 대소문자를 구분하지 않도록 한다.
 	 */
-    @Query("SELECT m.id as id, m.title as title, m.posterImage as posterImage, m.stDate as stDate, m.edDate as edDate FROM Musical m WHERE UPPER(m.title) LIKE CONCAT('%', UPPER(?1), '%')")
+    @Query("SELECT m.id as id, m.title as title, m.posterImage as posterImage, m.stDate as stDate, m.edDate as edDate FROM Musical m WHERE UPPER(m.title) LIKE CONCAT('%', UPPER(?1), '%') ORDER BY m.edDate DESC")
     List<SimpleMusicalDTO> findMusicalsByTitleKeyword(String keyword);
     
     // 배우 상세 페이지 해당 뮤지컬 포스터 조회
